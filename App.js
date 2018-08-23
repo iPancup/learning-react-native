d0c48baa4aee5eae15f5a5e94ccfae29b8e3a626
iPancup <ivanyusuf642@gmail.com>
2018-08-23 21:06:08 +0700

    perojekPertama

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Nama : Ivan Yusuf Satria</Text>
+        <Text>Kelas : XI RPL 4</Text>
+        <Text>No Absen : 18</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
