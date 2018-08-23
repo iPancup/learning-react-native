382b751ab1212246eb5fb4633c0eeee079cd4521
iPancup <ivanyusuf642@gmail.com>
2018-08-23 22:20:34 +0700

    langkah kedua

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Nama : Ivan Yusuf Satria</Text>
-        <Text>Kelas : XI RPL 4</Text>
-        <Text>No Absen : 18</Text>
+    render() {
+        return (
+          <View style={styles.container}>
+            <Text>Nama : Ivan Yusuf Satria</Text>
+            <Text>Kelas : XI RPL 4</Text>
+            <Text>No Absen : 18</Text>
+            <Image
+            style={{height:256,width:191}}
+            source={require('./1.jpg')}/>
       </View>
     );
   }
